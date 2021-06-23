670ecc6621af9af0f74630799182a3523ef160d4
Vsept17 <verzchseptian@gmail.com>
2021-06-23 21:32:06 +0700

    add bundle analyzer

--- webpack.common.js
+++ webpack.common.js
@@ -6,6 +6,7 @@ const ServiceWorkerWebpackPlugin = require("serviceworker-webpack-plugin");
 const path = require("path");
 const ImageminWebpackPlugin = require("imagemin-webpack-plugin").default;
 const ImageminMozjpeg = require("imagemin-mozjpeg");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 module.exports = {
   entry: path.resolve(__dirname, "src/scripts/index.js"),
@@ -55,5 +56,6 @@ module.exports = {
         }),
       ],
     }),
+    new BundleAnalyzerPlugin(),
   ],
 };
