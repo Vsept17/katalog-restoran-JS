d90b9f502602cf97ebd540103ba0131d2a1f5975
Vsept17 <verzchseptian@gmail.com>
2021-06-27 14:36:55 +0700

    tested by codecept

---
+++ e2e/Liking_Restaurant.spec.js
@@ -0,0 +1,22 @@
+/* eslint-disable quotes */
+/* eslint-disable no-undef */
+Feature("Liking Restaurant");
+
+const assert = require("assert");
+
+Scenario("Alur menyukai restaurant", async ({ I }) => {
+  I.amOnPage("/");
+  I.seeElement(".toDetail");
+
+  const firstFilm = locate(".toDetail").first();
+  const firstFIlmTitle = await I.grabTextFrom(firstFilm);
+  I.click(firstFilm);
+  I.seeElement(".detailResto");
+  I.click(".like");
+
+  I.amOnPage("/#/favorite");
+  I.seeElement(".favContainer");
+
+  const likedFilmTitle = await I.grabTextFrom(".infoResto a");
+  assert.strictEqual(firstFIlmTitle, likedFilmTitle);
+});
---
+++ e2e/Unlike_Reastaurant.spec.js
@@ -0,0 +1,35 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable quotes */
+/* eslint-disable linebreak-style */
+/* eslint-disable no-undef */
+Feature("Unlike Restaurant");
+
+const assert = require("assert");
+
+Scenario("Cancel Restaurant", async ({ I }) => {
+  I.amOnPage("/");
+  I.seeElement(".toDetail");
+  //   I.click(locate('.toDetail').first());
+
+  const firstFilm = locate(".toDetail").first();
+  const firstFIlmTitle = await I.grabTextFrom(firstFilm);
+  I.click(firstFilm);
+  I.seeElement(".detailResto");
+  I.click(".like");
+
+  I.amOnPage("/#/favorite");
+  I.seeElement(".favContainer");
+
+  const likedFilmTitle = await I.grabTextFrom(".infoResto a");
+  assert.strictEqual(firstFIlmTitle, likedFilmTitle);
+
+  I.seeElement(".list");
+  I.click(".nav__item_home");
+  I.seeElement(".list");
+  I.click(".nav__item_fav");
+  I.seeElement(".infoResto");
+  I.click(".infoResto a");
+
+  I.seeElement(".detailResto");
+  I.click(".like");
+});
